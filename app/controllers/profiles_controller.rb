8770a14cdc7561e16dd24bb0c4204193362f309d
Conrad Esche <conrad.3rd@gmail.com>
2022-08-30 15:21:36 +0200

    submit to complete dont work yet

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,42 @@
 class ProfilesController < ApplicationController
+  # skip_before_action :authenticate_user!, except: :new
+  before_action :set_offer, only: %i[show create edit update]
+
   def show
-    @user = current_user
+    @profile = current_user
   end
+
+
+  # def create
+  #   @profile = Profile.new(profile_params)
+  #   @profile.user = current_user
+
+  #   if @profile.save
+  #     redirect_to profile_path(@profile), notice: "Update ok"
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
+
+  # def edit
+  #   # @profile = Profile.update(current_user)
+  #   # @profile.user = current_user
+  # end
+
+  # def update
+  #   @profile.update(current_user)
+  #   redirect_to profile_path
+  # end
+
+  private
+
+  def set_offer
+    # @profile = Profile.find(params[:id])
+    @profile = current_user
+  end
+
+  # def profile_params
+  #   params.require(:profile).permit(:description, :first_name, :last_name)
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'profiles/show'
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  resources :profiles, only: %i[show]
-  # get '/profile', to: 'profiles#show'
+  # resources :profiles, only: %i[show edit update]
+  get '/profile', to: 'profiles#show'
+  # get '/profile/edit', to: 'profiles#edit'
 end
