96b81e9a83ace7654b75ad6159092c2f3df19777
Antonios Papatheodorou <antoniospapatheodorou@gmail.com>
2022-08-30 11:49:01 +0200

    routes and controller of creating an organisation

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,2 +1,24 @@
 class OrganisationsController < ApplicationController
+  skip_before_action :authenticate_user!, except: :new
+
+  def new
+    @organisation = Organisation.new
+  end
+
+  def create
+    @organisation = Organisation.new(organisation_params)
+    @organisation.user = current_user
+
+    if @organisation.save
+      redirect to organisation_path(@organisation), notice: "New organisation created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def organisation_params
+    params.require(:organisation).permit(:name, :address, :description, :photo, :website, :email, :first_name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+resources :organisations do
+  resources :donations, only: :create, :new
+end
   # Defines the root path route ("/")
   # root "articles#index"
 end
