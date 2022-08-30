8dfead8aa0cba299db53cc9cf5f9a84da9697e5e
Antonios Papatheodorou <antoniospapatheodorou@gmail.com>
2022-08-30 17:54:00 +0200

    donations controller and routes but view is missing

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,2 +1,29 @@
 class DonationsController < ApplicationController
+  before_action :set_organisation, only: %i[new create]
+
+  def new
+    @donation = Donation.new
+  end
+
+  def create
+    @donation = Donation.new(donation_params)
+    @donation.user = current_user
+    @donation.organisation = Organisation.find(params[:organisation_id])
+
+    if @donation.save
+      redirect_to profiles_path(@profile)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_donation
+    @donation = Donation.find(params[:id])
+  end
+
+  def donation_params
+    params.require(:donation).permit(:type_of_donation, :quantity)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :organisations, only: [ :new, :create, :edit, :update, :show, :index ] do
+  resources :organisations, only: [ :index, :new, :create, :edit, :update, :show ] do
 
-    resources :donations, only: :create
+    resources :donations, only: [ :new, :create]
   end
-  
+
   resources :users, only: [:update]
-  
+
   resources :donations
-  
+
   resources :profiles, only: %i[show]
-  
+
   get '/profile', to: 'profiles#show'
 end
