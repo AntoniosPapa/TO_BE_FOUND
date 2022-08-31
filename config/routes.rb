d81a58ab37da9b531809934cf61f20e5155f2c38
Esther Wynder <estherwynder@gmail.com>
2022-08-31 11:33:20 +0200

    add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
   resources :organisations, only: [ :index, :new, :create, :edit, :update, :show ] do
 
     resources :donations, only: [ :new, :create]
+
   end
 
   resources :users, only: [:update]
+
+  resources :skills_offers, only: %i[new create edit update]
+
 
   resources :donations
 
