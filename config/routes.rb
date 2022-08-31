b558f78e42c953fb9aa742c4994e052e75a1bf36
Conrad Esche <conrad.3rd@gmail.com>
2022-08-31 10:39:13 +0200

    clean up the routes.rb %i...

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :organisations, only: [ :index, :new, :create, :edit, :update, :show ] do
-
-    resources :donations, only: [ :new, :create]
+  resources :organisations, only: %i[index new create edit update show] do
+    resources :donations, only: %i[new create]
   end
 
   resources :users, only: [:update]
-
   resources :donations
-
   resources :profiles, only: %i[show]
-
   get '/profile', to: 'profiles#show'
 end
