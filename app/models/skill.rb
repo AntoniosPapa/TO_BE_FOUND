867dfa713b8ab4bdb005607c218260a04a85cf51
Esther Wynder <estherwynder@gmail.com>
2022-08-31 17:26:17 +0200

    Edited the edit box

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,4 @@
 class Skill < ApplicationRecord
   belongs_to :user
-  $skills = ["Photography", "Videography", "Construction Work", "Legal Advice" ]
+  $skills = ["Photography", "Videography", "Construction Work", "Legal Advice", "Web Development", "Translation"]
 end
