99356ea144bbd5f2c3d43ffad45755acb5adf8a0
Pauline Bewerunge <bewerungepauline@gmail.com>
2022-08-31 15:32:11 +0200

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ User.destroy_all
 
 puts "populate database with users.."
 
-user_photos =  ["https://images.pexels.com/photos/2269872/pexels-photo-2269872.jpeg?auto=compress&cs=tinysrgb&w=1200", "https://images.pexels.com/photos/2406949/pexels-photo-2406949.jpeg?auto=compress&cs=tinysrgb&w=1200", "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1200", "https://images.pexels.com/photos/1081685/pexels-photo-1081685.jpeg?auto=compress&cs=tinysrgb&w=1200", "https://images.pexels.com/photos/3746276/pexels-photo-3746276.jpeg?auto=compress&cs=tinysrgb&w=1200"]
+user_photos =  ["https://images.pexels.com/photos/2269872/pexels-photo-2269872.jpeg?auto=compress&cs=tinysrgb&w=1200", "https://images.pexels.com/photos/2406949/pexels-photo-2406949.jpeg?auto=compress&cs=tinysrgb&w=1200", "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1200", "https://images.pexels.com/photos/1081685/pexels-photo-1081685.jpeg?auto=compress&cs=tinysrgb&w=1200", "https://images.pexels.com/photos/3746276/pexels-photo-3746276.jpeg?auto=compress&cs=tinysrgb&w=1200", "https://i0.wp.com/news.globallandscapesforum.org/wp-content/uploads/2021/02/Sonia-Guajajara-nofilter.jpg?fit=444%2C592&ssl=1", "https://deolhonosruralistas.com.br/wp-content/uploads/2019/02/mulheresindigenas-kerexu.jpg"]
 
 user_photos.each do |user_photo|
   file = URI.open(user_photo)
@@ -38,29 +38,128 @@ user_photos.each do |user_photo|
 #   puts " Skills with #{skill.id} was successfully saved."
 end
 
-# puts "populate database with organiations.."
+puts "populate database with organisations.."
 
-# Organisation.create!([{
-#   name: "APIB - Articulation of Indigenous Peoples of Brazil",
-#   address: "Brazil",
-#   description: "Apib is an instance of national reference of the indigenous movement in Brazil,
-# created from the bottom up. It brings together our indigenous regional organizations and
-# was born with the purpose of strengthening the unity of our peoples, the articulation
-# between the different indigenous regions and organizations in the country,
-# in addition to mobilizing indigenous peoples and organizations against threats
-# and aggressions against indigenous rights.",
-#   website: "https://apiboficial.org/?lang=en"
+orga1 = Organisation.new(
+  name: "APIB - Articulation of Indigenous Peoples of Brazil",
+  address: "Brazil",
+  description: "Apib is an instance of national reference of the indigenous movement in Brazil,
+  created from the bottom up. It brings together our indigenous regional organizations and
+  was born with the purpose of strengthening the unity of our peoples, the articulation
+  between the different indigenous regions and organizations in the country,
+  in addition to mobilizing indigenous peoples and organizations against threats
+  and aggressions against indigenous rights.",
+  website: "https://apiboficial.org/?lang=en",
+  user_id: User.all.sample.id
+  )
+
+orga1.photo.attach(
+  io: URI.open("https://apiboficial.org/files/2022/01/C%C3%ADcero-Bezerra-_-@cicerone.bezerra-2-400x250.jpeg"),
+  filename: "#{orga1.name}.png",
+  content_type: "image/png"
+)
+orga1.save!
+
 # },
-# {
-#   name: "Minga Indigena",
-#   address: "Columbia",
-#   description: "",
-#   website: "https://www.youtube.com/watch?v=y4nt7MUahOc"
-# },
-# {
-#  name: "English",
-#   address: "Pete Doct",
-#   description: "C.",
-#   website:
-# }])
-# p "Created #{Book.count} Books"
+orga2 = Organisation.new(
+  name: "Amazon Watch",
+  address: "",
+  description: "Amazon Watch is a nonprofit organization founded in 1996 to protect
+  the rainforest and advance the rights of Indigenous peoples in the Amazon Basin in
+  Ecuador, Peru, Colombia, and Brazil. We work in solidarity with Indigenous and e
+  nvironmental organizations in campaigns for human rights, corporate accountability,
+  and the preservation of the Amazon’s ecological systems.
+
+  The Amazon is home to 400 distinct Indigenous peoples who are its best stewards.
+  The Amazon River Basin is the planet’s largest tropical rainforest.
+  A stabilizer of the global climate, it contains one-third of all species on Earth
+  and a large percentage of the world’s flowing fresh water. The forest and its peoples
+  are facing grave threats due to deforestation, resource extraction, land grabs, and
+  destructive development projects. At Amazon Watch, we are launching bold, strategic,
+  and timely actions to advance and amplify the solutions of Indigenous peoples.",
+
+  website: "https://amazonwatch.org/",
+  user_id: User.all.sample.id
+)
+
+orga2.photo.attach(
+  io: URI.open("https://amazonwatch.org/wp-content/uploads/2021/10/20.jpg"),
+  filename: "#{orga2.name}.png",
+  content_type: "image/png"
+)
+orga2.save!
+
+orga3 = Organisation.new(
+  name: "COIAB",
+  address: "Brasil",
+  description: "A missão da COIAB é defender os direitos dos povos indígenas a terra,
+  saúde, educação, cultura e sustentabilidade, considerando a diversidades de povos,
+  e visando sua autonomia através de articulação política e fortalecimento das
+  organizações indígenas.",
+  website: "https://coiab.org.br",
+  user_id: User.all.sample.id
+)
+
+orga3.photo.attach(io: URI.open("https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1657561892083x198606953326924350%2Fsite%2520copiarcafi.png?w=1024&h=589&auto=compress&dpr=2&fit=max"),  filename: "#{orga3.name}.png", content_type: "image/png")
+orga3.save!
+
+orga4 = Organisation.new(
+  name: "Instituto Raoni",
+  address: "Brasil",
+  description: "Nós, comunidades Mẽbêngôkre (ou Kayapó) criamos, em 2001, o Instituto Raoni
+  para defender os interesses do nosso povo. O IR é uma Organização da Sociedade Civil de
+    Interesse Público, sem fins lucrativos e de caráter associativo e apartidário localizada
+    na cidade de Peixoto de Azevedo (MT). Hoje representamos não apenas comunidades do povo Kayapó,
+      mas também dos povos Trumai, Tapayuna e Panará, para o acesso a políticas públicas e
+      captação de recursos para fortalecer nossas atividades culturais, nossa produção de
+      alimentos, nossa geração de renda e a proteção dos nossos territórios e direitos.",
+  website: "https://institutoraoni.org.br",
+  user_id: User.all.sample.id
+)
+
+orga4.photo.attach(
+  io: URI.open("https://institutoraoni.org.br/site/wp-content/uploads/2020/07/0718-nota-raoni-800x533.jpeg"),
+  filename: "#{orga3.name}.png",
+  content_type: "image/png"
+)
+orga4.save!
+
+orga5 = Organisation.new(
+  name: "Alianza Ceibo",
+  address: "Ecuador",
+  description: "Con las fuentes de agua contaminadas, los niños y niñas comenzaron a enfermar.
+  Por esa razón decidimos construir sistemas de agua lluvia para poder tomar agua pura en las comunidades.
+  Trabajamos entre cuatro nacionalidades y así nos fuimos conociendo. Vimos que todos teníamos problemas y
+  experiencias similares, y aunque somos distintos, nos dimos cuenta de que existen muchas cosas que nos unen.
+  De esta amistad surgió la “Alianza Ceibo”, que es una alianza conformada por personas de
+  los pueblos A’i Kofan, Siona, Siekopai y Waorani, que trabaja unida para llevar alegría y salud
+  a nuestras comunidades.",
+  website: "https://www.alianzaceibo.org/",
+  user_id: User.all.sample.id
+)
+
+orga5.photo.attach(
+  io: URI.open("https://www.alianzaceibo.org/m3di4/alianzafoto1.png"),
+  filename: "#{orga3.name}.png",
+  content_type: "image/png"
+)
+orga5.save!
+
+orga6 = Organisation.new(
+  name: "Mujeres Amazonicas",
+  address: "Amazonia",
+  description: "Somos un colectivo de Mujeres Amazónicas unidas con el único fin de defender la Selva,
+  la vida y nuestras culturas para las futuras generaciones. Formamos el grupo mujeres de 6 nacionalidades
+  indígenas de la Amazonia Ecuatoriana: Achuar, Shuar, Shiwiar, Kichwa, Sápara y Waorani.",
+  website: "https://www.mujeresamazonicas.net/",
+  user_id: User.all.sample.id
+)
+
+orga6.photo.attach(
+  io: URI.open("https://static.wixstatic.com/media/147184_5032913666c74ed28ffbc6ecd2ffcc2e~mv2.jpg/v1/fill/w_848,h_606,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Screenshot%202022-03-05%20211612_edited.jpg"),
+  filename: "#{orga3.name}.png",
+  content_type: "image/png"
+)
+orga6.save!
+
+p "Created #{Organisation.count} Organisations"
