d95504720ea260657ebf76f6d456f3dc86e01b85
Esther Wynder <estherwynder@gmail.com>
2022-08-31 11:47:56 +0200

    added new, create, edit, and update for users skills offered controller

--- app/controllers/skills_offers_controller.rb
+++ app/controllers/skills_offers_controller.rb
@@ -1,13 +1,25 @@
 class SkillsOffersController < ApplicationController
   def new
+    @skills_offers = SkillsOffers.new
   end
 
   def create
+    @skills_offers = SkillsOffers.new(skills_offers_params)
+    @skills_offers.user = current_user
   end
 
   def edit
+
   end
 
   def update
+    @skills_offers.update(skills_offers_params)
+    redirect_to profile_path
+  end
+
+  private
+
+  def skills_offers_params
+    params.require(:skills_offers).permit(:skills)
   end
 end
