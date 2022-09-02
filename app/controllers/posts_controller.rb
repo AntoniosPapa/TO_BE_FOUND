1eafb26d06279a4333a82d4638cb3018cf74671f
Conrad Esche <conrad.3rd@gmail.com>
2022-09-02 11:53:23 +0200

    not logged in user can see org posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  # skip_before_action :authenticate_user!, except: :new
+  skip_before_action :authenticate_user!, except: :new
 
   def index
     @posts = Post.all
